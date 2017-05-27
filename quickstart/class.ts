a2662c49b4af0434d11719f0211fd63bd0f80c78
zcge <zcge@Ctrip.com>
2017-05-27 17:59:21 +0800

    [Tag ID]:1:quickstart1;[Modify]:1:quickstart1;[Config]:N

--- quickstart/class.ts
+++ quickstart/class.ts
@@ -7,34 +7,34 @@ class Shape {
         this.color1 = 'pink';
     };
 
-    shoutout(){
-        return "I'm " + this.color1 + " " + this.name +  " with an area of " + this.area + " cm squared.";
+    shoutout() {
+        return "I'm " + this.color1 + " " + this.name + " with an area of " + this.area + " cm squared.";
     }
 }
 
 class Shape3D extends Shape {
- 
+
     volume: number;
- 
-    constructor ( public name: string, width: number, height: number, length: number ) {
-        super( name, width, height );
+
+    constructor(public name: string, width: number, height: number, length: number) {
+        super(name, width, height);
         this.volume = length * this.area;
     };
- 
-    shoutout() {
-        return "I'm " + this.name +  " with a volume of " + this.volume + " cm cube.";
+
+    shoutout(): string {
+        return "I'm " + this.name + " with a volume of " + this.volume + " cm cube.";
     }
- 
-    superShout() {
+
+    superShout(): string {
         return super.shoutout();
     }
 }
 
 var square = new Shape("square", 30, 30);
- 
-console.log( square.shoutout() );
-console.log( 'Area of Shape: ' + square.area );
-console.log( 'Name of Shape: ' + square.name );
-console.log( 'Color of Shape: ' + square.color1 );
-console.log( 'Width of Shape: ' + square.width );
-console.log( 'Height of Shape: ' + square.height );
+
+console.log(square.shoutout());
+console.log('Area of Shape: ' + square.area);
+console.log('Name of Shape: ' + square.name);
+console.log('Color of Shape: ' + square.color1);
+console.log('Width of Shape: ' + square.width);
+console.log('Height of Shape: ' + square.height);
